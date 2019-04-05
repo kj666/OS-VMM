9d4cd1d94ebcf10d78b703193204c7aca11e4d9b
mkanyar <33469024+mkanyar@users.noreply.github.com>
2019-04-04 23:04:56 -0400

    Api methods
    store,release,look, up

--- src/VMmanager.java
+++ src/VMmanager.java
@@ -1,16 +1,19 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
-
+import java.lang.*;
 public class VMmanager implements Runnable {
 
     private int maxSize;
     private int value;
     private String variableID;
-
-
+    ArrayList<String> memoryVariableID = new ArrayList<String>();
+    ArrayList<Integer> memoryVariableValue = new ArrayList<Integer>();
     @Override
     public void run() {
 
@@ -21,18 +24,26 @@ public class VMmanager implements Runnable {
         System.out.println("memory size: "+maxSize);
     }
 
-    public int store(String variableId, int value){
-        return 1;
+    public void store(String variableId, int value){
+        memoryVariableID.add(variableId);
+        memoryVariableValue.add(value);
     }
 
     public void release(String variableId){
+        memoryVariableID.remove(variableId);
+        memoryVariableValue.remove(memoryVariableID.indexOf(variableId));
 
     }
 
-    public int lookUp(String variableId){
-        return 1;
-    }
+    public int lookUp(String variableId) {
+        int variableIndex = memoryVariableID.indexOf(variableId);
+        if (variableIndex != -1) {
+            System.out.println("Variable " + memoryVariableID.get(variableIndex) + ", Value: " + memoryVariableValue.get(variableIndex));
+        } else {
 
+        }
+        return -1;
+    }
     public void swapMemory(){
 
     }
@@ -50,4 +61,6 @@ public class VMmanager implements Runnable {
         maxSize = scanner.nextInt();
 
     }
+
+
 }
